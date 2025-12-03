64d92c1773228fd7c6f3e50759cfbb74ac6f8dcc
estevesnp <nuno.pinto@live.com>
2025-12-03 23:03:42 +0000

    2025, day 1, part 1+2

--- src/day_01/part_1.rs
+++ src/day_01/part_1.rs
@@ -2,6 +2,51 @@
 const INPUT: &str = include_str!("input.txt");
 // const INPUT: &str = include_str!("small.txt");
 
+#[derive(Debug)]
+enum Side {
+    Left,
+    Right,
+}
+impl TryFrom<char> for Side {
+    type Error = String;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'L' => Ok(Side::Left),
+            'R' => Ok(Side::Right),
+            _ => Err(format!("invalid value: {value}")),
+        }
+    }
+}
+
 pub fn run() -> Result<usize, String> {
-    todo!()
+    let mut curr: usize = 50;
+    let mut count: usize = 0;
+
+    for line in INPUT.lines() {
+        let mut chars = line.chars();
+        let side = Side::try_from(chars.next().ok_or_else(|| "no char found".to_string())?)?;
+        let amnt: usize = chars
+            .as_str()
+            .parse()
+            .map_err(|e| format!("error parsing num: {e}"))?;
+        let amnt = amnt % 100;
+
+        match side {
+            Side::Left => {
+                if amnt > curr {
+                    curr = 100 - (amnt - curr);
+                } else {
+                    curr -= amnt;
+                }
+            }
+            Side::Right => curr = (curr + amnt) % 100,
+        }
+
+        if curr == 0 {
+            count += 1;
+        }
+    }
+
+    Ok(count)
 }
--- src/day_01/part_2.rs
+++ src/day_01/part_2.rs
@@ -2,6 +2,61 @@
 const INPUT: &str = include_str!("input.txt");
 // const INPUT: &str = include_str!("small.txt");
 
+#[derive(Debug)]
+enum Side {
+    Left,
+    Right,
+}
+impl TryFrom<char> for Side {
+    type Error = String;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'L' => Ok(Side::Left),
+            'R' => Ok(Side::Right),
+            _ => Err(format!("invalid value: {value}")),
+        }
+    }
+}
+
 pub fn run() -> Result<usize, String> {
-    todo!()
+    let mut curr: usize = 50;
+    let mut flips: usize = 0;
+
+    for line in INPUT.lines() {
+        let mut chars = line.chars();
+        let side = Side::try_from(chars.next().ok_or_else(|| "no char found".to_string())?)?;
+        let num: usize = chars
+            .as_str()
+            .parse()
+            .map_err(|e| format!("error parsing num: {e}"))?;
+
+        let amnt = num % 100;
+        flips += num / 100;
+
+        match side {
+            Side::Left => {
+                if amnt > curr {
+                    if curr != 0 {
+                        flips += 1;
+                    }
+                    curr = 100 - (amnt - curr);
+                } else {
+                    curr -= amnt;
+                    if curr == 0 {
+                        flips += 1;
+                    }
+                }
+            }
+            Side::Right => {
+                let sum = curr + amnt;
+                if sum > 99 {
+                    flips += 1;
+                }
+                curr = sum % 100
+            }
+        }
+    }
+
+    Ok(flips)
 }
